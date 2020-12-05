09b1711265c4b9b59f00c8b2c0f632c2cbf86a47
Nadiar Syaripul <codxse@gmail.com>
2020-12-05 14:23:26 +0700

    using primary and secondary constructor

--- kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
+++ kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
@@ -1,16 +1,7 @@
 import java.time.LocalDateTime
 import java.time.LocalDate
 
-class Gigasecond {
-    val date: LocalDateTime
-
-    constructor(date: LocalDate) {
-        this.date = date
-                .atStartOfDay()
-                .plusSeconds(1_000_000_000)
-    }
-
-    constructor(dateTime: LocalDateTime) {
-        this.date = dateTime.plusSeconds(1_000_000_000)
-    }
+class Gigasecond(dateTime: LocalDateTime) {
+    val date = dateTime.plusSeconds(1_000_000_000)
+    constructor(date: LocalDate): this(date.atStartOfDay())
 }
