acf94cd772246eb970155b7434a113c229f30218
Nadiar Syaripul <codxse@gmail.com>
2020-12-07 20:33:47 +0700

    reverse a string

---
+++ clojure/reverse-string/src/reverse_string.clj
@@ -0,0 +1,17 @@
+(ns reverse-string
+  (:require [clojure.spec.alpha :as s]))
+
+(s/fdef reverse-string
+        :args (s/cat :s string?)
+        :ret string?)
+
+(defn reverse-string [s]
+  (reduce (fn [acc z] (str z acc)) "" s))
+
+
+;; When I give `reverse-string` a vector input of int [1 2 3]
+;; it return 321,
+;; I thought it will throw an IllegalArgumentException,
+;; because, I specify that the args should be a string
+;;
+;; How to make that in only works for string arg with the spec?
